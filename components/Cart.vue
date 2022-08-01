a596a8047948fca97c46701baf1f7890e9c26c56
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-08-01 16:22:07 +0530

    first commit

---
+++ components/Cart.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="cartBg" class=" bg-fuchsia-300 border-4 border-black w-1/4">
+<div class=" font-bold text-xl p-5"> Start Time {{startTime}}</div>
+<div  class=" font-bold text-xl p-5"> End Time {{endTime}}</div>
+<div><button id="btn" @click="book()" class=" bg-lime-400 border-2 p-2 border-black rounded-md ml-20 mb-5 hover:bg-lime-500">Book Now</button></div>
+</div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+         data:new Date().toLocaleTimeString()
+
+      }
+   },
+
+   props:{
+    startTime: String,
+    endTime:String,
+    id:String,
+   } ,
+    methods: {
+    book() {
+      alert(this.data) ;
+
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2 class=" align-middle">Welcome  Make our Apporment</h2>
+    <div class="flex flex-wrap justify-around gap-5">
+      <Cart id="a1" startTime="9:04:00 AM" endTime="9:10:00 AM"></Cart>
+      <Cart id="a1" startTime="9:10:00 AM" endTime="9:20:00 AM"></Cart>
+      <Cart id="a1" startTime="9:20:00 AM" endTime="9:30:00 AM"></Cart>
+      <Cart id="a1" startTime="9:30:00 AM" endTime="9:40:00 AM"></Cart>
+      <Cart id="a1" startTime="9:40:00 AM" endTime="9:50:00 AM"></Cart>
+      
+       <Cart id="a1" startTime="9:50:00 AM" endTime="10:00:00 AM"></Cart>
+      <Cart id="a1" startTime="10:00:00 AM" endTime="10:10:00 AM"></Cart>
+      <Cart id="a1" startTime="10:10:00 AM" endTime="10:20:00 AM"></Cart>
+      <Cart id="a1" startTime="10:20:00 AM" endTime="10:30:00 AM"></Cart>
+      <Cart id="a1" startTime="10:30:00 AM" endTime="10:40:00 AM"></Cart>
+
+       <Cart id="a1" startTime="10:40:00 AM" endTime="10:50:00 AM"></Cart>
+      <Cart id="a1" startTime="10:50:00 AM" endTime="11:00:00 AM"></Cart>
+      <Cart id="a1" startTime="11:00:00 AM" endTime="11:10:00 AM"></Cart>
+      <Cart id="a1" startTime="11:10:00 AM" endTime="11:20:00 AM"></Cart>
+      <Cart id="a1" startTime="11:20:00 AM" endTime="11:30:00 AM"></Cart>
+
+       <Cart id="a1" startTime="11:30:00 AM" endTime="11:40:00 AM"></Cart>
+      <Cart id="a1" startTime="11:40:00 AM" endTime="11:50:00 AM"></Cart>
+      <Cart id="a1" startTime="11:50:00 AM" endTime="12:00:00 PM"></Cart>
+      <Cart id="a1" startTime="12:00:00 AM" endTime="12:10:00 PM"></Cart>
+      <Cart id="a1" startTime="12:10:00 AM" endTime="12:20:00 PM"></Cart>
+
+       <Cart id="a1" startTime="12:20:00 AM" endTime="12:30:00 PM"></Cart>
+      <Cart id="a1" startTime="12:30:00 AM" endTime="12:40:00 PM"></Cart>
+      <Cart id="a1" startTime="12:40:00 AM" endTime="12:50:00 PM"></Cart>
+      <Cart id="a1" startTime="12:50:00 AM" endTime="1:00:00 PM"></Cart>
+      <Cart id="a1" startTime="1:00:00 AM" endTime="1:10:00 PM"></Cart>
+    </div>
+  </div>
+</template>
+<script>
+import Cart from "../components/Cart.vue";
+
+export default {
+  components: {
+    Cart,
+  },
+};
+</script>
