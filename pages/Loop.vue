946901bc6310fbe82496d1e122c4aa2756aa8872
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-08-02 18:40:08 +0530

    screen add

--- pages/Loop.vue
+++ pages/Loop.vue
@@ -1,4 +1,28 @@
 <template>
+  <div id="hide">
+    <form class="flex flex-col w-1/2 m-auto" v-on:submit="preventDefault()">
+      <label class="m-5" for="valisName"
+        >name : <input type="text" id="valisName" v-model.lazy="cName" required
+      /></label>
+      <label class="m-5" for="num"
+        >mobail:
+        <input type="number" id="num" v-model.lazy="cNum" maxlength="10" required
+      /></label>
+      <div class="flex">
+        <button
+          @click="booked()"
+          class="w-1/3 border-2 m-1 border-black rounded-md hover:bg-red-700"
+        >
+          save</button
+        ><button
+          @click="cancel"
+          class="w-1/3 border-2 m-1 border-black rounded-md hover:bg-red-700"
+        >
+          cancel
+        </button>
+      </div>
+    </form>
+  </div>
   <div>
     <div>
       <button
@@ -10,31 +34,20 @@
     </div>
     <div class="flex flex-wrap justify-between gap-4">
       <div
-        class="bg-fuchsia-300 border-4 border-black w-1/4"
+        class="bg-green-700 border-4 border-black w-1/4"
         v-for="(item, i) in timeSlots"
         v-bind:id="i"
       >
         <div class="font-bold text-xl p-5">Start Time {{ item.startTime }}</div>
         <div class="font-bold text-xl p-5">End Time {{ item.endTime }}</div>
-        <div>
-          <button
-            @click="book(i)"
-            class="bg-lime-400 border-2 p-2 border-black rounded-md ml-20 mb-5 hover:bg-lime-500"
-          >
-            Book Now
-          </button>
-        </div>
-      </div>
-    </div>
 
-    <div id="hide">
-      <label for="name">name : <input type="text" id="name" required /></label>
-      <label for="phone">mobail: <input required type="number" /></label>
-      <button @click="booked()" class="border-2 border-black rounded-md hover:bg-red-700">
-        save</button
-      ><button @click="cancel" class="border-2 border-black rounded-md hover:bg-red-700">
-        cancel
-      </button>
+        <button
+          @click="book(i)"
+          class="bg-lime-400 border-2 p-2 border-black rounded-md ml-20 mb-5 hover:bg-lime-500"
+        >
+          Book Now
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -44,6 +57,8 @@ export default {
     return {
       timeSlots: [],
       valis: false,
+      cName: "",
+      cnum: "",
     };
   },
   methods: {
@@ -52,8 +67,25 @@ export default {
     },
 
     booked() {
+      var s = document.getElementById("valisName").value;
+      var cn = document.getElementById("num").value;
+
+      if (s == "" && cn == "") {
+        alert("name should not empty");
+      }
+      this.cName = s;
+      this.cnum = cn;
       document.getElementById("hide").style = "display:none";
       this.valis = true;
+    },
+    book(i) {
+      document.getElementById("hide").style = " visibility: visible;";
+      var s = document.getElementById(i);
+      if (this.valis == true) {
+        s.style = " background-color: rgb(238, 15, 15);";
+        s.lastElementChild.innerHTML = "EditSlot";
+        location.href = "#hide";
+      }
     },
     showSlots() {
       document.getElementById("hide").style = "display:none";
@@ -71,18 +103,6 @@ export default {
         });
       }
     },
-    book(i) {
-      document.getElementById("hide").style = " visibility: visible;";
-      var s = document.getElementById(i);
-      s.style = " background-color: rgb(97, 222, 222)";
-    },
   },
 };
 </script>
-<style scoped>
-#hide {
-  position: absolute;
-  top: 30px;
-  left: 10px;
-}
-</style>
